1446f01220c7a70b98011f91b8776234874fb8f9
Jamie Git Git <justicehopelove@yahoo.com>
2019-02-26 13:23:24 -0800

    did jinja in html for menu. current output/input files innacurate

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 #-get rid of search bar
 #- get rid of made with heart
 #- make photos smaller
+#- change input and output files to be diff
 
 
 #import statements -----------
@@ -42,8 +43,11 @@ def create_page_list():
         file_dict = {
             "filename": page,
             "output": "docs/"+ name_only + ".html",
+            "doc_name":name_only + ".html",
             "title": name_only,
         }
+        if page == "content/index.html":
+            file_dict["title"]="Home"
         list_of_pages.append(file_dict)
     for html_page in list_of_pages: #just for my use. will delete
         print(html_page)    
@@ -51,25 +55,20 @@ def create_page_list():
 #generate a menu from the list_of_pages 
 def create_menu(pages):
     header_footer = open("templates/headerfooter.html").read()
-    menu = ""
     #generate menu link for each page
     for page in pages:
         title = page["title"]
-        caps_title=title.capitalize()
-        if title == "index":
-            caps_title = "Home"
-            template_file= open("content/index.html").read()
-            output_file = "content/index.html"
+        if title == "Home":
+            template_file= open("content/index.html").read() #i can't make input and output file be same
+            output_file = "content/indexwmenu.html"
         else:
             template_file = header_footer
-            output_file = "templates/headerfooter.html"
-        menu_item = '<li class="nav-item">\n\t<a class="nav-link" href="'+ title + '.html">' +caps_title+'</a>\n</li>'
-        menu = menu + menu_item
+            output_file = "templates/headerfooterwmenu.html"
         #place menu in  template
         template = Template(template_file) 
         menu_inserted = template.render( #this actually renders templates/headerfooter.html twice. is that good? bad?
-            menu_items= menu,
-        )  
+            pages= pages,
+        ) 
         open(output_file, "w+").write(menu_inserted)
     
 
@@ -83,6 +82,7 @@ def create_full_base():
     template = Template(base_template)
     full_base=template.render(
         header_footer=header_footer,
+        title ="{{title| title}}", #this pass destroys the title, so have to re-insert it
     )  
     open("templates/fullbase.html", "w+").write(full_base)     
        
@@ -90,7 +90,7 @@ def create_full_base():
 def assign_base(page_name):
     base = None
     #index gets basic base, others get full base    
-    if page_name == "index":
+    if page_name == "Home":
         base = open("templates/base.html").read()
     else:
         base = open("templates/fullbase.html").read()
@@ -100,12 +100,12 @@ def assign_base(page_name):
 #replace placeholder in each page with the page's content, title, year
 def assemble_page(page_name, page_template, filename, output):
     # Define content, title, and year
-    if page_name == "index":
-        title = "Home"
+    if page_name == "Home":
+       # title = "Home"
         index = open(filename).read()
         content = None
     else:
-        title= page_name.capitalize()
+        #title= page_name.capitalize()
         content = open(filename).read()
         index = None
     now = datetime.datetime.now()
@@ -115,7 +115,7 @@ def assemble_page(page_name, page_template, filename, output):
     template = Template(page_template)
     finished_page=template.render(
         content = content,
-        title = title,
+        title = page_name,
         index= index,
         year = year,
     )  
