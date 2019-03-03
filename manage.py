01e56d150e7b0559b7d64a2ff35f3e0ce2a01c7e
Jamie Git Git <justicehopelove@yahoo.com>
2019-03-03 15:36:08 -0800

    fixed style

--- manage.py
+++ manage.py
@@ -1,6 +1,30 @@
 import utils
+import sys
+
 
 
 #run--------------    
-if __name__ == "__main__":
-    utils.main()
+if __name__ == "__main__":   
+    print("This is argv:", sys.argv)
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == "build":
+            print("Build was specified")
+            utils.build()
+            print("Your content pages are now templated into your website design")
+        elif command == "new":
+            print("New page was specified")
+            utils.new()
+            print("A new content page has been created at content/new.html")
+        else:
+            print("Usage:")
+            print("  To rebuild site: python manage.py build")
+            print("  To create new page: python manage.py new")
+    else:
+        print("Usage:")
+        print("  To rebuild site: python manage.py build")
+        print("  To create new page: python manage.py new") #DRY... :/
+
+    
+    
+
--- utils.py
+++ utils.py
@@ -1,94 +1,103 @@
-# todo:
-#- make photos smaller
-#-change jinja file to make a class for when I'm on that page. then css it
 
-
-#import statements -----------
+# import statements -----------
 import datetime
 import glob
 import os
 from jinja2 import Template
 
-#lists---------------
+# lists---------------
 list_of_pages = []
 
 
-#functions--------------
+# functions--------------
+def build():
+    main()
+
+
 def main():
     create_list_of_pages()
     create_interior_files()
-    #assemble each page 
+    # assemble each page
     for page in list_of_pages:
-        assemble_page(page["title"], page["stage"], page["output"])               
+        assemble_page(page["title"], page["stage"], page["output"])
 
-#build the list of pages automatically
+
+# build the list of pages automatically
 def create_list_of_pages():
     all_html_files = glob.glob("content/*.html")
     for page in all_html_files:
-        print(page) #delete later
         file_name = os.path.basename(page)
         name_only, extension = os.path.splitext(file_name)
         file_dict = {
             "title": name_only,
-            "doc_name":name_only + ".html",
+            "doc_name": name_only + ".html",
             "input": page,
-            "stage": "interior/"+ name_only+".html",
-            "output": "docs/"+ name_only + ".html",
+            "stage": "interior/" + name_only + ".html",
+            "output": "docs/" + name_only + ".html",
         }
         if page == "content/index.html":
-            file_dict["title"]="Home"
+            file_dict["title"] = "Home"
         list_of_pages.append(file_dict)
-    for html_page in list_of_pages: #just for my use. will delete
-        print(html_page)    
 
-#put content inside header template, but just copy index over
+
+# put content inside header template, but just copy index over
 def create_interior_files():
     for page in list_of_pages:
-        print(page["title"], page["input"], page["stage"]) #delete
         # copy index.html from contents to interior
-        page_content=open(page["input"]).read()
+        page_content = open(page["input"]).read()
         if page["title"] == "Home":
-            interior_file=page_content
-        # sandwich all other pages between header and footer
+            interior_file = page_content
+        # template all other pages between header and footer
         else:
-            header_footer=open("templates/headerfooter.html").read()
+            header_footer = open("templates/headerfooter.html").read()
             template = Template(header_footer)
             interior_file = template.render(
                 content=page_content,
-                #place jinja code to create menu
-                menu=menu_jinja(),
-                year= "{{year}}",
+                year="{{year}}",
+                menu="{{menu}}",
             )
         open(page["stage"], "w+").write(interior_file)
 
+
 # import jinja code to make menu
 def menu_jinja():
-    make_menu= open("templates/menucode.html.j2").read()
+    make_menu = open("templates/menucode.html.j2").read()
     return make_menu
 
-       
-            
-#replace placeholder in each page with the page's content, title, year
+
+# replace placeholders in each page with the content, title, year, menu jinja code
 def assemble_page(page_name, interior_file, output):
-    #add menu and date to interior files
-    interior= open(interior_file).read()
+    # add menu code and date to interior files
+    interior = open(interior_file).read()
     now = datetime.datetime.now()
-    year=str(now.year)
-    template=Template(interior)
-    templated_interior=template.render(
-        pages=list_of_pages, 
-        year= year,
+    year = str(now.year)
+    template = Template(interior)
+    templated_interior = template.render(
+        year=year,
+        # place jinja code to create menu
+        menu=menu_jinja(),
     )
-    # put interior files in base
-    base= open("templates/base.html").read()
+    # autogenerate menu
+    template = Template(templated_interior)
+    templated_interior = template.render(
+        pages=list_of_pages,
+        page_name=page_name,
+    )
+    # put interior files in base,
+    if page_name == "Home":
+        page_name = "index"
+    base = open("templates/base.html").read()
     template = Template(base)
-    finished_page=template.render(
-        content = templated_interior,
-        title = page_name,
-    )  
-    open(output, "w+").write(finished_page)    
-
-        
+    finished_page = template.render(
+        content=templated_interior,
+        title=page_name,
+    )
+    open(output, "w+").write(finished_page)
 
 
-    
+def new():
+    blank_content = '''
+        <div>
+            <p> Insert content here </p>
+        </div>'''
+    open("content/new.html", "w+").write(blank_content)
